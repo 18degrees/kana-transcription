49c8c620f40770f9a6bed3a5acbd4654ef859f35
18degrees <mikhaili.dev@gmail.com>
2024-07-14 23:56:39 +0300

    added tests for EN transformToKana()

--- tests/en/transformToKana-test.js
+++ tests/en/transformToKana-test.js
@@ -1,12 +1,40 @@
 import { transformToKana } from '../../lib/esm/index.mjs'
 import { assert } from "chai"
 
-describe('TransformToKana function: transforms russian syllables to japanese syllabary',  () => {
+describe('TransformToKana function: transforms english syllables to japanese syllabary',  () => {
     describe('Examples of work and disadvantages', () => {
         it('devoiced vowel', () => {
             // Подробнее: п.4 и п.5 в /docs/explanation.md
 
             assert.equal(transformToKana('skoshii'), 'すこしい')
+        })
+        it('long vowel', () => {
+            /*
+                Подробнее: п.2 в /docs/explanation.md
+            */
+            assert.equal(transformToKana('kookoo'), 'こおこお') 
+
+            assert.equal(transformToKana('ookii'), 'おおきい')
+
+            assert.equal(transformToKana('ee go'), 'ええ ご')
+        })
+        it('long consonant', () => {
+            //Подробнее: п.4 в /docs/explanation.md
+
+            assert.equal(transformToKana('motto'), 'もっと')
+        })
+        it('`na` row and `n` syllable', () => {
+            //Подробнее: п.3 в /docs/explanation.md
+            
+            assert.equal(transformToKana("ten'in"), 'てんいん')
+            assert.equal(transformToKana('minna'), 'みんな')
+        })
+    
+        it('long vowel (to katakana)', () => {
+            /*
+                Подробнее: п.2 в /docs/explanation.md
+            */
+            assert.equal(transformToKana('keeki', 'en', 'katakana'), 'ケエキ');
         })
     })
     describe('Test-core', () => {
