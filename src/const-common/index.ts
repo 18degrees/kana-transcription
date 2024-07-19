5740b84320b73b3b45d35e5d5352a2d40b466512
18degrees <mikhaili.dev@gmail.com>
2024-07-19 09:17:07 +0300

    docs: fix typo

--- src/const-common/index.ts
+++ src/const-common/index.ts
@@ -1,1 +1,10 @@
 export const spacesRegExp = /[ 　]/g    //учитывается обычной пробемел и тот, что ставится на японской раскладке
+
+export const kanaRegExp = /\p{Script=Kana}{1}|\p{Script=Hira}{1}|ー/ug
+
+export const jpPunctuationMarksRegExp = /[（）｛｝［］【】、，゠…‥。「」『』〝〟：！？〽♪・￥〒]/g
+export const spaceBeforeMarksRegExp = /[（｛［【「『〝〽]/
+export const spaceAfterMarksRegExp = /[）｝］】」』〟、，。…‥！？♪]/
+export const spaceAroundMarksRegExp = /[・￥〒]/
+
+export const kanaAndJpPunctuationMarksRegExp = /\p{Script=Kana}{1}|\p{Script=Hira}{1}|ー|[ 　（）｛｝［］【】、，]/ug
